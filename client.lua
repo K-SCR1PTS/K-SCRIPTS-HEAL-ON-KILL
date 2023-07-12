027992fc2104ab6946a06481c4e7242b0d8ad82c
K-SCRIPTS <139262676+K-SCR1PTS@users.noreply.github.com>
2023-07-12 17:54:24 +0200

    Add files via upload

---
+++ client.lua
@@ -0,0 +1,11 @@
+
+
+RegisterNetEvent('k_healonkill:client:heal', function ()
+    print('c')
+    local ped = PlayerPedId();
+    print(ped)
+    print(GetEntityMaxHealth(ped))
+    SetEntityHealth(ped, GetEntityMaxHealth(ped));
+
+    -- 'You have been healed'-notification?
+end)
